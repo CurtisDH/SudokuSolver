4eefd7cb808c430291a7ee0cf8ec0439c2a80b8e
CurtisDH <curtisdh0@gmail.com>
2023-06-29 10:38:36 +1000

    Initial project setup

---
+++ SudokuSolver/SudokuGenerator.cs
@@ -0,0 +1,8 @@
+namespace SudokuSolver;
+
+// This will be used for generating and testing Sudoku games, and potentially turn into a parser for images
+public class SudokuGenerator
+{
+    // 9x9 square must be filled in with numbers from 1-9 with no repeated numbers in each line horizontally or vertically
+    // There are also 3x3 squares marked out in the grid, each of these squares cannot have repeating numbers either
+}
